f97808577a3f8d161e7d50a00d87385e3ee15511
Andrey F. Mindubaev <covex.mobile@gmail.com>
2012-11-04 18:09:40 +0300

    Checkstyle warnings: * ControlStructures

--- library/JooS/Config.php
+++ library/JooS/Config.php
@@ -1,212 +1,233 @@
 <?php
 
- /**
-  * @package JooS
-  */
-  class JooS_Config implements ArrayAccess, Iterator {
-    const CLASS_PREFIX = "JooSX_Config";
-    
-    /**
-     * @var array
-     */
-    private $_data;
-    
-    private $_className = null;
+/**
+ * @package JooS
+ */
+class JooS_Config implements ArrayAccess, Iterator
+{
 
-    private $_root = null;
+  const CLASS_PREFIX = "JooSX_Config";
 
-    private $_modified = null;
+  /**
+   * @var array
+   */
+  private $_data;
+  private $_className = null;
+  private $_root = null;
+  private $_modified = null;
+  private static $_instances = array();
 
-    private static $_instances = array();
+  protected function __construct(&$data)
+  {
+    $this->_data = &$data;
+    reset($this);
+  }
 
-    protected function __construct(&$data) {
-      $this->_data = &$data;
-      reset($this);
-    }
+  /**
+   *
+   * @param string $name
+   * @return JooS_Config
+   */
+  public static function getInstance($name)
+  {
+    require_once "JooS/Loader.php";
 
-    /**
-     *
-     * @param string $name
-     * @return JooS_Config
-     */
-    public static function getInstance($name) {
-      require_once "JooS/Loader.php";
-
-      $className = self::getClassName($name);
-      if (!isset(self::$_instances[$className])) {
-        if (JooS_Loader::loadClass($className)) {
-          $config = new $className();
-        }
-        else {
-          $data = array();
-          $config = new self($data);
-        }
-        
-        /* @var $config JooS_Config */
-        $config->_className = $className;
-        $config->_root = $config;
-
-        self::$_instances[$className] = $config;
-      }
-      return self::$_instances[$className];
-    }
-
-    /**
-     *
-     * @param string $name
-     */
-    public static function clearInstance($name) {
-      $className = self::getClassName($name);
-      if (isset(self::$_instances[$className])) {
-        unset(self::$_instances[$className]);
-      }
-    }
-
-    /**
-     *
-     * @param string $name
-     * @return string
-     */
-    public static function getClassName($name) {
-      require_once "JooS/Loader.php";
-
-      return JooS_Loader::getClassName(self::CLASS_PREFIX, $name, true);
-    }
-
-    /**
-     *
-     * @param string $name
-     * @param array $data
-     * @return JooS_Config
-     */
-    public static function newInstance($name, $data = null) {
-      if (!is_array($data)) {
+    $className = self::getClassName($name);
+    if (!isset(self::$_instances[$className])) {
+      if (JooS_Loader::loadClass($className)) {
+        $config = new $className();
+      } else {
         $data = array();
+        $config = new self($data);
       }
 
-      $config = self::getInstance($name);
-      $config->_data = $data;
+      /* @var $config JooS_Config */
+      $config->_className = $className;
+      $config->_root = $config;
 
-      return $config;
+      self::$_instances[$className] = $config;
     }
+    return self::$_instances[$className];
+  }
 
-    /**
-     * @return array
-     */
-    public function valueOf() {
-      return $this->_data;
-    }
-
-    /**
-     * @return string
-     */
-    public function get_class() {
-      return $this->_className;
-    }
-
-    /**
-     * @return bool
-     */
-    public function is_modified() {
-      return $this->_root->_modified;
-    }
-
-    public function __get($key) {
-      if ($this->__isset($key)) {
-        $config =  new self($this->_data[$key]);
-        $config->_root = $this->_root;
-      }
-      else {
-        $config = null;
-      }
-
-      return $config;
-    }
-
-    public function __set($key, $value) {
-      if (!is_array($this->_data)) {
-        trigger_error("Cannot use a scalar value as an array", E_USER_NOTICE); return;
-      }
-
-      if (is_array($value) || is_scalar($value)) {
-        $new_value = $value;
-      }
-      elseif (is_object($value) && $value instanceof JooS_Config) {
-        $new_value = $value->valueOf();
-      }
-      else {
-        trigger_error("Type mismatch", E_USER_NOTICE); return;
-      }
-
-      $this->_data[$key] = $new_value;
-      $this->_root->_modified = true;
-    }
-
-    public function __isset($key) {
-      return is_array($this->_data) && isset($this->_data[$key]) ? true : false;
-    }
-
-    public function __unset($key) {
-      if ($this->__isset($key)) {
-        unset($this->_data[$key]);
-        $this->_root->_modified = true;
-      }
-    }
-
-    public function __call($name, $param) {
-      $config = $this->__get($name);
-      return is_null($config) ? null : $config->valueOf();
-    }
-
-    public function __invoke() {
-      return $this->valueOf();
-    }
-
-    /**
-     *
-     * @param string $name
-     * @param array $arguments
-     * @return JooS_Config
-     */
-    public static function __callStatic($name, $arguments) {
-      return self::getInstance($name);
-    }
-
-    public function offsetGet($key) {
-      return $this->__get($key);
-    }
-
-    public function offsetSet($key, $value) {
-      $this->__set($key, $value);
-    }
-
-    public function offsetExists($key) {
-      return $this->__isset($key);
-    }
-
-    public function offsetUnset($key) {
-      $this->__unset($key);
-    }
-
-    public function current() {
-      return $this->__get($this->key());
-    }
-
-    public function key() {
-      return key($this->_data);
-    }
-
-    public function next() {
-      list(, $next) = each($this->_data);
-      return $this->current();
-    }
-
-    public function rewind() {
-      reset($this->_data);
-    }
-
-    public function valid() {
-      $key = $this->key();
-      return is_null($key) ? false : isset($this->_data[$key]);
+  /**
+   *
+   * @param string $name
+   */
+  public static function clearInstance($name)
+  {
+    $className = self::getClassName($name);
+    if (isset(self::$_instances[$className])) {
+      unset(self::$_instances[$className]);
     }
   }
+
+  /**
+   *
+   * @param string $name
+   * @return string
+   */
+  public static function getClassName($name)
+  {
+    require_once "JooS/Loader.php";
+
+    return JooS_Loader::getClassName(self::CLASS_PREFIX, $name, true);
+  }
+
+  /**
+   *
+   * @param string $name
+   * @param array $data
+   * @return JooS_Config
+   */
+  public static function newInstance($name, $data = null)
+  {
+    if (!is_array($data)) {
+      $data = array();
+    }
+
+    $config = self::getInstance($name);
+    $config->_data = $data;
+
+    return $config;
+  }
+
+  /**
+   * @return array
+   */
+  public function valueOf()
+  {
+    return $this->_data;
+  }
+
+  /**
+   * @return string
+   */
+  public function get_class()
+  {
+    return $this->_className;
+  }
+
+  /**
+   * @return bool
+   */
+  public function is_modified()
+  {
+    return $this->_root->_modified;
+  }
+
+  public function __get($key)
+  {
+    if ($this->__isset($key)) {
+      $config = new self($this->_data[$key]);
+      $config->_root = $this->_root;
+    } else {
+      $config = null;
+    }
+
+    return $config;
+  }
+
+  public function __set($key, $value)
+  {
+    if (!is_array($this->_data)) {
+      trigger_error("Cannot use a scalar value as an array", E_USER_NOTICE);
+      return;
+    }
+
+    if (is_array($value) || is_scalar($value)) {
+      $new_value = $value;
+    } elseif (is_object($value) && $value instanceof JooS_Config) {
+      $new_value = $value->valueOf();
+    } else {
+      trigger_error("Type mismatch", E_USER_NOTICE);
+      return;
+    }
+
+    $this->_data[$key] = $new_value;
+    $this->_root->_modified = true;
+  }
+
+  public function __isset($key)
+  {
+    return is_array($this->_data) && isset($this->_data[$key]) ? true : false;
+  }
+
+  public function __unset($key)
+  {
+    if ($this->__isset($key)) {
+      unset($this->_data[$key]);
+      $this->_root->_modified = true;
+    }
+  }
+
+  public function __call($name, $param)
+  {
+    $config = $this->__get($name);
+    return is_null($config) ? null : $config->valueOf();
+  }
+
+  public function __invoke()
+  {
+    return $this->valueOf();
+  }
+
+  /**
+   *
+   * @param string $name
+   * @param array $arguments
+   * @return JooS_Config
+   */
+  public static function __callStatic($name, $arguments)
+  {
+    return self::getInstance($name);
+  }
+
+  public function offsetGet($key)
+  {
+    return $this->__get($key);
+  }
+
+  public function offsetSet($key, $value)
+  {
+    $this->__set($key, $value);
+  }
+
+  public function offsetExists($key)
+  {
+    return $this->__isset($key);
+  }
+
+  public function offsetUnset($key)
+  {
+    $this->__unset($key);
+  }
+
+  public function current()
+  {
+    return $this->__get($this->key());
+  }
+
+  public function key()
+  {
+    return key($this->_data);
+  }
+
+  public function next()
+  {
+    list(, $next) = each($this->_data);
+    return $this->current();
+  }
+
+  public function rewind()
+  {
+    reset($this->_data);
+  }
+
+  public function valid()
+  {
+    $key = $this->key();
+    return is_null($key) ? false : isset($this->_data[$key]);
+  }
+
+}
--- library/JooS/Loader.php
+++ library/JooS/Loader.php
@@ -1,81 +1,88 @@
 <?php
 
- /**
-  * @package JooS
-  */
-  class JooS_Loader {
-    /**
-     * @param string $className
-     * @return bool
-     */
-    public static function loadClass($className) {
-      if (!class_exists($className)) {
-        $path = self::getPath($className);
+/**
+ * @package JooS
+ */
+class JooS_Loader
+{
 
-        $include_paths = explode(PATH_SEPARATOR, get_include_path());
-        for ($i=0; $i<sizeof($include_paths); $i++) {
-          $realPath = $include_paths[$i] . DIRECTORY_SEPARATOR . $path;
+  /**
+   * @param string $className
+   * @return bool
+   */
+  public static function loadClass($className)
+  {
+    if (!class_exists($className)) {
+      $path = self::getPath($className);
 
-          if (file_exists($realPath)) {
-            require_once($realPath);
-            
-            return class_exists($className);
-          }
+      $include_paths = explode(PATH_SEPARATOR, get_include_path());
+      for ($i = 0; $i < sizeof($include_paths); $i++) {
+        $realPath = $include_paths[$i] . DIRECTORY_SEPARATOR . $path;
+
+        if (file_exists($realPath)) {
+          require_once($realPath);
+
+          return class_exists($className);
         }
-        return false;
       }
-      else {
-        return true;
-      }
-    }
-
-    /**
-     * @param string $className
-     * @param string $ext
-     * @return string
-     */
-    public static function getPath($className, $ext = "php") {
-      return str_replace("_", DIRECTORY_SEPARATOR, $className) . "." . $ext;
-    }
-
-    /**
-     * @param string $className
-     * @param string $ext
-     * @return string
-     */
-    public static function getRelativePath($className, $ext = "php") {
-      require_once "JooS/Config.php";
-
-      $library = JooS_Config::PHPackager_Library()->path() ?: "library";
-      return $library . DIRECTORY_SEPARATOR . self::getPath($className, $ext);
-    }
-
-    /**
-     * @param string $className
-     * @param string $ext
-     * @return string
-     */
-    public static function getRealPath($className, $ext = "php") {
-      require_once "JooS/Config.php";
-
-      return JooS_Config::PHPackager()->path() . DIRECTORY_SEPARATOR . self::getRelativePath($className, $ext);
-    }
-
-    /**
-     * @param string $prefix
-     * @param string $name
-     * @param bool $ucfirst
-     * @return string
-     */
-    public static function getClassName($prefix, $name, $ucfirst = false) {
-      if ($ucfirst) {
-        $path = explode("_", $name);
-        for ($i=0; $i<sizeof($path); $i++) {
-          $path[$i] = ucfirst(strtolower($path[$i]));
-        }
-        $name = implode("_", $path);
-      }
-
-      return ($prefix ? $prefix ."_" : "") . $name;
+      return false;
+    } else {
+      return true;
     }
   }
+
+  /**
+   * @param string $className
+   * @param string $ext
+   * @return string
+   */
+  public static function getPath($className, $ext = "php")
+  {
+    return str_replace("_", DIRECTORY_SEPARATOR, $className) . "." . $ext;
+  }
+
+  /**
+   * @param string $className
+   * @param string $ext
+   * @return string
+   */
+  public static function getRelativePath($className, $ext = "php")
+  {
+    require_once "JooS/Config.php";
+
+    $library = JooS_Config::PHPackager_Library()->path() ? : "library";
+    return $library . DIRECTORY_SEPARATOR . self::getPath($className, $ext);
+  }
+
+  /**
+   * @param string $className
+   * @param string $ext
+   * @return string
+   */
+  public static function getRealPath($className, $ext = "php")
+  {
+    require_once "JooS/Config.php";
+
+    return JooS_Config::PHPackager()->path() . DIRECTORY_SEPARATOR . self::getRelativePath($className, $ext);
+  }
+
+  /**
+   * @param string $prefix
+   * @param string $name
+   * @param bool $ucfirst
+   * @return string
+   */
+  public static function getClassName($prefix, $name, $ucfirst = false)
+  {
+    if ($ucfirst) {
+      $path = explode("_", $name);
+      for ($i = 0; $i < sizeof($path); $i++) {
+        $path[$i] = ucfirst(strtolower($path[$i]));
+      }
+      $name = implode("_", $path);
+    }
+
+    return ($prefix ? $prefix . "_" : "") . $name;
+  }
+
+}
