2200d3771a7233e920b1b57eb869878af6782072
Andrey F. Mindubaev <covex.mobile@gmail.com>
2012-11-07 16:23:35 +0300

    PEAR coding standart test

--- library/JooS/Event/Exception.php
+++ library/JooS/Event/Exception.php
@@ -2,11 +2,11 @@
 
 /**
  * PHPackager
- * 
- * @author Andrey F. Mindubaev <andrey.mindubaev@gmail.com>
- * @category PHPackager
- * @package JooS
- * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ *
+ * @category JooS
+ * @package  JooS
+ * @author   Andrey F. Mindubaev <andrey.mindubaev@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 /** @see JooS_Exception */
@@ -14,11 +14,11 @@ require_once "JooS/Exception.php";
 
 /**
  * JooS_Event_Exception
- * 
- * @author Andrey F. Mindubaev <andrey.mindubaev@gmail.com>
- * @category PHPackager
- * @package JooS
- * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ *
+ * @category JooS
+ * @package  JooS
+ * @author   Andrey F. Mindubaev <andrey.mindubaev@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 class JooS_Event_Exception extends JooS_Exception
